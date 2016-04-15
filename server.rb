8e8eda721135d92b7894cecedbbdb1a4c43ef708
itsironhawk <itsironhawk@gmail.com>
2016-04-15 11:20:21 -0400

    updates UI, cleans code

--- server.rb
+++ server.rb
@@ -10,7 +10,7 @@ require 'nokogiri'
 #require_relative 'webscraper.rb'
 get '/' do
   @post_lyrics = ""
-  erb :"index.html"
+  erb :index
 end
 
 get '/search' do
@@ -18,62 +18,43 @@ get '/search' do
   sng = params[:title].to_s
   artist = art.gsub(/\s/,'-')
   title = sng.gsub(/\s/,'-')
-  erb :"search.html"
+  @lyrics = $lyrics
+  @word = $wrd
+  @songNames = $songNames
+  erb :search
 end
 
 post "/search" do
+  $lyricToScrape = ""
+  $lyrics = ""
+  $titleLink = ""
   $songNames = []
   $songUrls = []
   $wrd = params[:word].to_s
   word = $wrd.gsub(/\s/,'%20').downcase
-  puts word
   page = HTTParty.get('http://www.lyrics.net/lyrics/' + word)
-  url = ('http://www.lyrics.net/lyrics/' + word)
-  puts url
   parse_page = Nokogiri::HTML(page)
   parse_page.css('.lyric-meta-title a[href]').map do |a|
-    #puts a.href
-    puts a['href']
-    #$titleUrl = a["href"]
     $songUrls.push(a['href'])
-    #$songUrls.push(a["href"])
-    #puts $songNames
     $lyricToScrape = $songUrls[0]
-    #put $lyricToScrape
-    puts "Its #{$songUrls[0]}"
   end
+
   parse_page.css('.lyric-meta-title').each do |a|
-    puts a.text
-    #puts a
     $titleLink = a.text
-    #$titleUrl = a["href"]
     $songNames.push(a.text)
-    #$songUrls.push(a["href"])
-    #puts $songNames
-    #$lyricToScrape = $songUrls[0]
-    #put $lyricToScrape
   end
+
   firstLink = HTTParty.get('http://www.lyrics.net' + $lyricToScrape)
-  puts ('http://www.lyrics.net' + $lyricToScrape)
   parse_page = Nokogiri::HTML(firstLink)
   parse_page.css('#lyric-body-text').each do |a|
     $lyrics = a.text
     $lyrics = $lyrics.to_s
     $final_lyrics = $lyrics.gsub! '\n', '</br>'
-    puts $lyrics
-    puts $final_lyrics
   end
+
   parse_page.css('embed[src]').map do |a|
-    #puts a.href
-    puts a['src']
     $video = a['src']
-    #$titleUrl = a["href"]
-    #$songUrls.push(a['href'])
-    #$songUrls.push(a["href"])
-    #puts $songNames
-    #$lyricToScrape = $songUrls[0]
-    #put $lyricToScrape
-    puts "Its #{a['src']}"
   end
+
   redirect to '/search'
 end
